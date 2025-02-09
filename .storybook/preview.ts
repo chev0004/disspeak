a5bfa741e225ec269800be06f9cf55a866d8b057
chev <chevv0004@gmail.com>
2025-02-09 13:20:51 +0900

    feat: update storybook settings

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import '@/app/globals.css';
 
 const preview: Preview = {
   parameters: {
@@ -7,6 +8,15 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: 'var(--color-background-main)',
+        },
+      ],
     },
   },
 };
